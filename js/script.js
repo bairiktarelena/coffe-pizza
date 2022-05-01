9ad0754bf5d7b3d629e252879533223987d730ad
bairiktarelena <104640701+bairiktarelena@users.noreply.github.com>
2022-05-01 11:15:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function functie() {
+    document.getElementById("et").style.fontSize = "60px";    
+}
+/*II*/
+function functie1() { document.getElementById("et1").innerHTML = "";}    
+
+function functie2() { document.getElementById("et2").innerHTML = "Надеюсь все поняли!!!???";}
+/*III prompt*/    
+
+function ravno(){
+    var text;
+    var rezultat = prompt("2+2*2");
+    if (rezultat==6) { text = "Молодец-правильно!"; }
+    else { text = "Внимательнее - неправильно!";  }
+    document.getElementById("rez").innerHTML = text;
+}
+/* переменные*/
+function name(params) {
+
+
+    var nume = "Иванов"
+    var nume = "Иван"
+    var varsta =35;
+    document.getElementById("et1").innerHTML = nume;
+    document.getElementById("et2").innerHTML = prenume;
+    document.getElementById("et3").innerHTML = varsta;
+}
+function compar() {
+    var x = 5;
+    document.getElementById("et6").innerHTML = (x === 5)
+}
+
+
+
+
+
+    
+
+
